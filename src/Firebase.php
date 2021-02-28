583ad5f3e6a379a92039e81bb191a4fc47ee2678
krishna mishra <er.krishna.mishra@gmail.com>
2021-02-28 21:38:24 +0530

    first commit

---
+++ src/Firebase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Simplexi\Firebase;
+
+class Firebase
+{
+    public function firebase(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
